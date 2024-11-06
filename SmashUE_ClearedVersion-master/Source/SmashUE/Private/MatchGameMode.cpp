7e4f91346b49b7342fe5bf66a6cfb675cab31908
clément bostyn <clement.bostyn@edu.devinci.fr>
2024-11-06 11:41:45 +0100

    add first commit w project

---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Private/MatchGameMode.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MatchGameMode.h"
+
+#include "Editor.h"
+#include "Arena/ArenaPlayerStart.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMatchGameMode::FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultActors)
+{
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AArenaPlayerStart::StaticClass(), FoundActors);
+	for(int i = 0; i < FoundActors.Num();++i)
+	{
+		AArenaPlayerStart* ArenaPlayerStartActor = Cast<AArenaPlayerStart>(FoundActors[i]);
+		if(ArenaPlayerStartActor == nullptr) continue;
+		ResultActors.Add(ArenaPlayerStartActor);
+	}
+}
+
+void AMatchGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	TArray<AArenaPlayerStart*> PlayerStartsPoints;
+	FindPlayerStartActorsInArena(PlayerStartsPoints);
+	for(AArenaPlayerStart* PlayerStartsPoint : PlayerStartsPoints)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Cyan, PlayerStartsPoint->GetFName().ToString());
+	}
+}
+
---
+++ SmashUE_ClearedVersion-master/Source/SmashUE/Public/MatchGameMode.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MatchGameMode.generated.h"
+
+/**
+ * 
+ */
+
+class AArenaPlayerStart;
+UCLASS()
+class SMASHUE_API AMatchGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	virtual void BeginPlay() override;
+private:
+	void FindPlayerStartActorsInArena(TArray<AArenaPlayerStart*>& ResultActors);
+};
